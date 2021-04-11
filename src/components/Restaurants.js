403afa4bd276fd787d2466c51b6801d55562796c
gracenak <gracenak@gmail.com>
2021-04-10 20:02:18 -0700

    commit tree e004fb5bd8aff84feeec3cbafaeeace6662fc1c1

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
 class Restaurants extends Component {
 
   render() {
@@ -20,5 +19,6 @@ class Restaurants extends Component {
 const mapStateToProps = state => {
   return { restaurants: state.restaurants }
 }
+
 
 export default connect(mapStateToProps)(Restaurants);
